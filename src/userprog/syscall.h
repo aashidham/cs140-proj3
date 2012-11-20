adaf574ddf76d17ace3450335a41a2d65c309a96
Aaditya Ganesh Shidham <ashidham@stanford.edu>
2012-11-19 19:15:19 -0800

    Sundaram proj2 commit

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,13 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include <list.h>
 
 void syscall_init (void);
-
+void exit (int status);
+struct file_elem
+{
+	int fd;
+	struct file *file_pointer;
+	struct list_elem elem;
+};
 #endif /* userprog/syscall.h */
